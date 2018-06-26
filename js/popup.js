3a5e4234d197df7092c5788473cc1ba9343ecfcf
Kyungwook Cho <amisay5256@naver.com>
2018-06-26 17:17:51 +0900

    change path of images, refactoring - popup.js

--- js/popup.js
+++ js/popup.js
@@ -3,56 +3,29 @@
 // dateNow: Date 객체. 출력되어 있는 날짜 정보를 담고 있음.
 // menu: dateNow의 메뉴 정보를 담고 있는 객체.
 
-let meal = {date: new Date(), menu: new Object()};
-updateMenu(true);
+let currentMealInfo = {date: new Date(), menu: new Object()};
+updateMenu();
 
 document.getElementById("button-yesterday").addEventListener("click", onClickYesterday);
 document.getElementById("button-tomorrow").addEventListener("click", onClickTomorrow);
 document.getElementById("button-size").addEventListener("click", onClickSize);
 
-chrome.storage.local.get("size", function(items) {
-    let isInited = false;
-
-    if(items.size == undefined) chrome.storage.local.set({size: "12px"}, function() {
-        changeSize(newSize);
-    });
-    else changeSize(items.size);
+getFontSizeFromChromeLocalStorage(function(fontSize) {
+    changeFontSize(fontSize);
+    changeFontSizeButtonText(fontSize);
 });
 
-function onClickYesterday() {
-    meal.date.setDate(meal.date.getDate() - 1);
-    updateMenu(true);
-}
-
-function onClickTomorrow() {
-    meal.date.setDate(meal.date.getDate() + 1);
-    updateMenu(true);
-}
-
-function onClickSize() {
-    chrome.storage.local.get("size", function(items) {
-        let newSize = "12px";
-        switch(items.size) {
-            case "8px": newSize = "12px"; break;
-            case "12px": newSize = "16px"; break;
-            case "16px": newSize = "8px"; break;
-        }
-
-        chrome.storage.local.set({size: newSize});
-        changeSize(newSize);
+function updateMenu() {
+    getMenuFromAPI(currentMealInfo.date, function(menu) {
+        currentMealInfo.menu = menu;
+        printMenu();
     });
 }
 
-function changeSize(size)
-{
-    document.getElementsByTagName("html")[0].style.fontSize = size;
-    document.getElementById("button-size").innerHTML = size;
-}
-
-function updateMenu(print) {
-    let year = meal.date.getFullYear().toString();
-    let month = (meal.date.getMonth() + 1).toString();
-    let date = meal.date.getDate().toString();
+function getMenuFromAPI(menuDate, callback) {
+    let year = menuDate.getFullYear().toString();
+    let month = (menuDate.getMonth() + 1).toString();
+    let date = menuDate.getDate().toString();
 
     month = month.length == 1 ? "0" + month : month;
     date = date.length == 1 ? "0" + date : date;
@@ -61,8 +34,8 @@ function updateMenu(print) {
 
     axios.get(url)
     .then(response => {
-        meal.menu = JSON.parse(JSON.stringify(response.data));
-        if(print == true) printMenu();
+        menu = JSON.parse(JSON.stringify(response.data));
+        callback(menu);
     })
     .catch(err => {
         console.log(err);
@@ -70,9 +43,9 @@ function updateMenu(print) {
 }
 
 function printMenu() {
-    let month = (meal.date.getMonth() + 1).toString();
-    let date = meal.date.getDate().toString();
-    let day = ['일', '월', '화', '수', '목', '금', '토'][meal.date.getDay()];
+    let month = (currentMealInfo.date.getMonth() + 1).toString();
+    let date = currentMealInfo.date.getDate().toString();
+    let day = ['일', '월', '화', '수', '목', '금', '토'][currentMealInfo.date.getDay()];
 
     document.getElementById("date").innerHTML = `${month}월 ${date}일 ${day}요일`;
 
@@ -84,15 +57,69 @@ function printMenu() {
     lunch.innerHTML = "";
     dinner.innerHTML = "";
 
-    for (item of meal.menu.breakfast) {
+    for (item of currentMealInfo.menu.breakfast) {
         breakfast.innerHTML += '<li><span class="text">' + item + '</span></li>';
     }
 
-    for (item of meal.menu.lunch) {
+    for (item of currentMealInfo.menu.lunch) {
         lunch.innerHTML += '<li><span class="text">' + item + '</span></li>';
     }
 
-    for (item of meal.menu.dinner) {
+    for (item of currentMealInfo.menu.dinner) {
         dinner.innerHTML += '<li><span class="text">' + item + '</span></li>';
     }
+}
+
+function onClickYesterday() {
+    currentMealInfo.date.setDate(currentMealInfo.date.getDate() - 1);
+    updateMenu();
+}
+
+function onClickTomorrow() {
+    currentMealInfo.date.setDate(currentMealInfo.date.getDate() + 1);
+    updateMenu();
+}
+
+function onClickSize() {
+    getFontSizeFromChromeLocalStorage(function(fontSize) {
+        let newFontSize = getLargerFontSize(fontSize);
+        setFontSizeToChromeLocalStorage(newFontSize, function(fontSize) {
+            changeFontSize(fontSize);
+            changeFontSizeButtonText(fontSize);
+        });
+    });
+}
+
+function getFontSizeFromChromeLocalStorage(callback) {
+    chrome.storage.local.get("fontSize", function(items) {
+        if(items.fontSize === undefined) {
+            setFontSizeToChromeLocalStorage("12px", callback);
+        } else {
+            callback(items.fontSize);
+        }
+    });
+}
+
+function setFontSizeToChromeLocalStorage(value, callback) {
+    chrome.storage.local.set({fontSize: value}, function() {
+        callback(value);
+    });
+}
+
+function changeFontSize(fontSize) {
+    document.getElementsByTagName("html")[0].style.fontSize = fontSize;
+}
+
+function changeFontSizeButtonText(fontSize) {
+    document.getElementById("button-size").innerHTML = fontSize;
+}
+
+function getLargerFontSize(fontSize) {
+    let newFontSize;
+    switch(fontSize) {
+        case "8px": newFontSize = "12px"; break;
+        case "12px": newFontSize = "16px"; break;
+        case "16px": newFontSize = "8px"; break;
+    }
+    return newFontSize;
 }
